6b40df64d7301fe0ecb97b4ce433d23fb534fada
laker <njlaker@gmail.com>
2016-08-19 01:25:23 +0800

    [config] fix add run scipt multitime bug

--- LAFramework/runscript.rb
+++ LAFramework/runscript.rb
@@ -2,10 +2,22 @@ require 'xcodeproj'
 class RunScriptConfigurator
     def self.post_install(installer)
         project = installer.aggregate_targets.first.user_project
-        project.targets do |target|
-            phase = target.new_shell_script_build_phase("Annotation Script")
-            phase.shell_script = "Pods/LAFramework/LANetworking/Scripts/generate.sh\nPods/LAFramework/LAJsonKit/Scripts/json_generate.sh"
+        project.targets.each do |target|
+            phases = target.shell_script_build_phases
+            do_break = false
+            phases.each do |phase|
+                if (phase.name == "Annotation Script")
+                    do_break=true
+                    break
+                end
+            end
+            next if do_break
+            newPhase = target.new_shell_script_build_phase("Annotation Script")
+            newPhase.shell_script = "Pods/LAFramework/LANetworking/Scripts/generate.sh\nPods/LAFramework/LAJsonKit/Scripts/json_generate.sh"
         end
         project.save()
     end
+
+
+    
 end
